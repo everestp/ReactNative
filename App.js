2dec8b92cd86ec987fbc44df567a404d58111a43
Everest Paudel <paudeleverest09@gmail.com>
2025-03-08 13:05:53 +0545

    Learnig :- Images

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image  source={require("./assets/download (1).jpeg")} style={{
+        width:"100%",
+       height:"200",
+       
+      }}/>
+      <Image source={require("./assets/download.jpeg")}  style={{
+        width:150,
+        height:150,
+        borderRadius:150,
+        borderColor : "black",
+        borderWidth:5,
+        marginTop:-75
+        }}/> 
       <Text style={{ fontSize:20 }}>Everest Paudel</Text>
+      <Text>React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+ 
---
+++ assets/page/image.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import 
+const image = () => {
+  return (
+    <View style={styles.container}>
+    <Image /> 
+      <Text>
+
+         
+      </Text>
+    </View>
+  )
+}
+
+export default image
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
