8d4b545dabd317bc924c1bfb3d69290819e0314b
Everest Paudel <paudeleverest09@gmail.com>
2025-03-08 16:21:22 +0545

    Learning :SafeAreaView

--- App.js
+++ App.js
@@ -26,91 +26,97 @@ export default function App() {
     Linking.openURL("https://github.com/everestp");
     console.log("Contact me");
   };
-  return (
+  // return (
 
-  <SafeAreaProvider>
-    <SafeAreaView  edge={['top']} style={{ backgroundColor:"red"}}>
-        <View
-          style={{
-            backgroundColor: "palegreen",
-            height: "100%",
-            alignItems: "center",
-          }}
-        >
-          <Text style={{ fontSize: 100 }}> Tittle </Text>
-          <DisplayInsets/>
-          <Text style={{ marginTop: "auto" }}>Footer</Text>
-        </View>
-      </SafeAreaView>
-  </SafeAreaProvider>
+  // <SafeAreaProvider>
+  //   <SafeAreaView  edge={['top']} style={{ backgroundColor:"red"}}>
+  //       <View
+  //         style={{
+  //           backgroundColor: "palegreen",
+  //           height: "100%",
+  //           alignItems: "center",
+  //         }}
+  //       >
+  //         <Text style={{ fontSize: 100 }}> Tittle </Text>
+  //         <DisplayInsets/>
+  //         <Text style={{ marginTop: "auto" }}>Footer</Text>
+  //       </View>
+  //     </SafeAreaView>
+  // </SafeAreaProvider>
   
     
-  );
-  // return (
-  //   <ScrollView showsVerticalScrollIndicator={false}>
-
-  //   <View style={styles.container}>
-  //     <Image  source={require("./assets/download (1).jpeg")} style={{
-  //       width:"100%",
-  //      height:"200",
-
-  //     }}/>
-  //     <Image source={require("./assets/download.jpeg")}  style={{
-  //       width:150,
-  //       height:150,
-  //       borderRadius:150,
-  //       borderColor : "black",
-  //       borderWidth:5,
-  //       marginTop:-75
-  //       }}/>
-  //     <Text style={{ fontSize:20 }}>Everest Paudel</Text>
-  //     <Text>React Native</Text>
-
-  //     <View style={{ flexDirection:"row" ,marginVertical :10,gap:10}}>
-  //     <FontAwesome6 name="github" size={24} color="balck" />
-  //     <FontAwesome6 name="x-twitter" size={24} color="black" />
-  //     <FontAwesome6 name="at" size={24} color="black" />
-  //     </View>
-  //     <Button title='Contact me' onPress={()=> onContactMe()}/>
-  //     <Text>
-  //       lorem ipsum dolor sit amet, consectetur adipiscing elit.
-  //       Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-  //       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-  //       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-  //       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  //       lorem ipsum dolor sit amet, consectetur adipiscing elit.
-  //       Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-  //       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-  //       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-  //       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  //       lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.
-  //       Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-  //       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-  //       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-  //       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  //       lorem ipsum dolor sit amet, consectetur adipiscing elit.
-  //       Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-  //       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-  //       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-  //       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  //       lorem ipsum dolor sit amet, consectetur adipiscing elit.
-  //       Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-  //       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-  //       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-  //       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  //       lorem ipsum dolor sit amet, consectetur adipiscing elit.
-  //       Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-  //       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-  //       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-  //       Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  //       lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.
-  //       Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-  //     </Text>
-
-  //     <StatusBar style="auto" />
-  //   </View>
-  //   </ScrollView>
   // );
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView edges={['bottom']}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+
+<View style={styles.container}>
+  <Image  source={require("./assets/download (1).jpeg")} style={{
+    width:"100%",
+   height:"200",
+
+  }}/>
+  <Image source={require("./assets/download.jpeg")}  style={{
+    width:150,
+    height:150,
+    borderRadius:150,
+    borderColor : "black",
+    borderWidth:5,
+    marginTop:-75
+    }}/>
+  <Text style={{ fontSize:20 }}>Everest Paudel</Text>
+  <Text>React Native</Text>
+
+  <View style={{ flexDirection:"row" ,marginVertical :10,gap:10}}>
+  <FontAwesome6 name="github" size={24} color="balck" />
+  <FontAwesome6 name="x-twitter" size={24} color="black" />
+  <FontAwesome6 name="at" size={24} color="black" />
+  </View>
+  <Button title='Contact me' onPress={()=> onContactMe()}/>
+  <Text>
+    lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    lorem ipsum dolor sit amet, consectetur adipiscing elit.lorem ipsum dolor sit amet, consectetur adipiscing elit.
+    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  </Text>
+
+  <StatusBar style="auto" />
+</View>
+</ScrollView>
+      </SafeAreaView>
+
+    </SafeAreaProvider>
+  
+  );
 }
 
 const styles = StyleSheet.create({
